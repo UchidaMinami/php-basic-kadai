6b9ded1cd8bfa813460d179a9eb920c71a763538
UchidaMinami <potechi0373@gmail.com>
2023-06-21 02:10:02 +0000

    課題提出００７　連想配列

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題2 連想配列</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+
+         
+         $food_data = ['name' => 'orion', 'price' => 200, 'weight' => 160];
+         
+         print_r($food_data);
+         
+         echo '<br>';
+         
+
+
+         ?>
+     </p>
+    
+ </body>
+ 
+ </html>
